02ce91e6b2f7f953da44c553d6a888d2356680ed
Iddhi Sulakshana <iddhidasanayaka@gmail.com>
2023-01-03 21:59:08 +0530

    Finished index

--- js/home.js
+++ js/home.js
@@ -2,3 +2,18 @@ const cover =  document.getElementById('hero');
 window.onload = () => {
     cover.style.height = (cover.scrollHeight - nav.scrollHeight) + 'px';
 }
+
+const cards = document.querySelectorAll('.card');
+cards.forEach(cardHoverEvent)
+function cardHoverEvent(card){
+    const cardTimeLine = gsap.timeline();
+    cardTimeLine.to(card.children[1], {transform: "translateY(0%)", duration: 0.5});
+    cardTimeLine.pause();
+    card.addEventListener('mouseover', () => {
+        cardTimeLine.play();
+
+    })
+    card.addEventListener('mouseout', () => {
+        cardTimeLine.reverse();
+    });
+}
